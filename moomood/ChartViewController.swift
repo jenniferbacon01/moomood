77b7b751182a25dd60c0a8856ed3157d09331dfb
Jennifer Bacon <jenniferbacon01@gmail.com>
2017-10-16 13:31:45 +0100

    added second chart

--- moomood/ChartViewController.swift
+++ moomood/ChartViewController.swift
@@ -125,8 +125,140 @@ class ChartViewController: UIViewController {
         
     }
     
+   
+    
+    @IBOutlet weak var barChart: BarChartView!
+//    var exampleRatings: [Double] = [1,2,3,4,5]
+//    var matchedRatings: [String] = []
+//    var exampleQuantity: [Double] = [7,6,3,4,10]
+    
+    
+    
+    func setUpBarChart (){
+        
+        
+        let realm = try! Realm()
+        let moods = realm.objects(MoodDB.self)
+        var count1 : Int = 0
+        var count2 : Int = 0
+        var count3 : Int = 0
+        var count4 : Int = 0
+        var count5 : Int = 0
+        
+        for i in (0..<moods.count){
+            if moods[i].rating == 1 {
+                count1 += 1
+            }
+        }
+        for i in (0..<moods.count){
+            if moods[i].rating == 2 {
+                count2 += 1
+            }
+        }
+        for i in (0..<moods.count){
+            if moods[i].rating == 3 {
+                count3 += 1
+            }
+        }
+        for i in (0..<moods.count){
+            if moods[i].rating == 4 {
+                count4 += 1
+            }
+        }
+        for i in (0..<moods.count){
+            if moods[i].rating == 5 {
+                count5 += 1
+            }
+        }
+        let frequencies: [Int] = [count1, count2, count3, count4, count5]
+        let barRatings: [Int]! = [1,2,3,4,5]
+        
+            
+            
+            //set up line array
+            var barChartEntry = [BarChartDataEntry]()
+            
+            for i in (0..<frequencies.count){
+                let value = BarChartDataEntry(x: Double(i), y: Double(frequencies[i]))
+                barChartEntry.append(value)
+            }
+            
+            print(barChartEntry)
+            
+            let frequencyBar = BarChartDataSet(values: barChartEntry, label: "Frequency")
+            formatGraph(frequencyBar: frequencyBar)
+            let data = BarChartData()
+            
+            data.addDataSet(frequencyBar)
+            
+            barChart.data = data
+            
+//            barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values:[String(barRatings)])
+            barChart.xAxis.axisMinimum = -0.5
+            barChart.xAxis.axisMaximum = Double(barChartEntry.count) - 0.5;
+            barChart.xAxis.axisMaximum = 4.5
+            barChart.xAxis.granularityEnabled = true
+            barChart.xAxis.granularity = 1.0
+            barChart.setVisibleXRangeMaximum(5)
+            data.barWidth = 0.80
+            
+            // show the latest entries
+            //        let when = DispatchTime.now() + 0.1
+            //        DispatchQueue.main.asyncAfter(deadline: when) {
+            //            self.barChart.moveViewToX(Double(moods.count - 1))
+            //        }
+        }
+    
+    func formatGraph (frequencyBar: BarChartDataSet){
+        frequencyBar.colors = [NSUIColor.purple]
+        frequencyBar.drawValuesEnabled = false
+        
+        //remove xAxis gridlines
+        barChart.xAxis.drawGridLinesEnabled = false
+        
+        //remove yAxis gridlines
+        barChart.leftAxis.drawGridLinesEnabled = false
+        barChart.rightAxis.drawGridLinesEnabled = false
+        
+        //remove xAxis
+        barChart.xAxis.drawAxisLineEnabled = false
+        
+        //keep left yAxis
+        barChart.leftAxis.drawAxisLineEnabled = true
+        barChart.rightAxis.drawAxisLineEnabled = false
+        
+        //remove description
+        barChart.chartDescription?.text = ""
+        
+        // label at the bottom
+        barChart.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        
+        // remove right y axis
+        barChart.rightAxis.enabled = false
+        
+        // pinch zoom enabled
+        barChart.pinchZoomEnabled = true
+        
+        // font size of x and y axis labels
+        barChart.xAxis.labelFont = UIFont(name: "HelveticaNeue-Light", size: 10)!
+        barChart.leftAxis.labelFont = UIFont(name: "HelveticaNeue-Light", size: 10)!
+        
+        
+        // make sure x axis labels are visible
+        barChart.xAxis.avoidFirstLastClippingEnabled = true
+        
+        // setting scale and range on y axis
+        barChart.leftAxis.axisMinimum = 0
+        barChart.leftAxis.granularityEnabled = true
+        barChart.leftAxis.granularity = 1.0
+        
+    }
+    
+        
+       
     override func viewDidAppear(_ animated: Bool) {
         setUpChart()
+        setUpBarChart()
        
     }
     
