9283010a4606b06cedd40f314b5140cc8a03a073
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-10-17 15:21:02 +0100

    analysis basic page

--- moomood/AnalysisViewController.swift
+++ moomood/AnalysisViewController.swift
@@ -13,23 +13,37 @@ class AnalysisViewController: UIViewController {
 
     @IBOutlet weak var happyOverviewMessage: UILabel!
     @IBOutlet weak var sadOverviewMessage: UILabel!
-    var happyMessage: String = ""
-    var sadMessage: String = ""
+    var happyMessage: [String] = []
+    var sadMessage: [String] = []
     
+    @IBOutlet weak var average: UILabel!
     
     func setUpOverviews (){
         let realm = try! Realm()
         let moods = realm.objects(Mood.self)
         print(moods)
-        for i in (0..<moods.count){
-            if moods[i].rating > 3 {
-                happyMessage.append(moods[i].moodDescription)
-            } else if moods[i].rating < 3 {
-                sadMessage.append(moods[i].moodDescription)
+        var sumOfMoods: Double = 0.0
+        var startMoods: Double = 0.0
+        if moods.count < 10 {
+            startMoods = Double(moods.count)
+        } else {
+            startMoods = 10
+        }
+        for i in (moods.count - Int(startMoods)..<moods.count){
+            sumOfMoods += Double(moods[i].rating)
+            if moods[i].moodDescription != "" {
+                if moods[i].rating > 3 {
+                    happyMessage.append(moods[i].moodDescription)
+                } else if moods[i].rating < 3 {
+                    sadMessage.append(moods[i].moodDescription)
+                }
             }
         }
-        happyOverviewMessage.text = happyMessage
-        sadOverviewMessage.text = sadMessage
+        happyOverviewMessage.text = (happyMessage.map{String(describing: $0)}).joined(separator: ", ")
+        sadOverviewMessage.text = (sadMessage.map{String(describing: $0)}).joined(separator: ", ")
+        let unroundedAverage = sumOfMoods/startMoods
+        let roundedAverage = Double(round(100*unroundedAverage)/100)
+        average.text = String(roundedAverage)
     }
     
     override func viewDidLoad() {
