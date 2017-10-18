cfc352a5e31be986c1353a9bd3a8563ad0373ead
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-10-18 12:27:12 +0100

    Enable access to photo library in info.plist to enable image saving in camera roll
    - Addition to info.plist: NSPhotoLibraryUsageDescription, NSPhotoLibraryAddUsageDescription
    - Added a toolbar with saveToLibrary outlet in ShowMediaViewController

--- moomood/ShowMediaViewController.swift
+++ moomood/ShowMediaViewController.swift
@@ -3,9 +3,28 @@ import UIKit
 class ShowMediaViewController: UIViewController {
     var image: UIImage? = nil
     var titreText: String!
+  
+    @IBAction func saveToLibrary(_ sender: UIBarButtonItem) {
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    //MARK: - Add image to Library
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
     
     @IBOutlet weak var imageView: UIImageView!
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if image != nil {
