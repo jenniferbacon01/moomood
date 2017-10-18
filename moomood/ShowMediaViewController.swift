629ef9a73a54fac84467c7132c7553310adb60b1
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-10-18 12:34:13 +0100

    Revised message alert when the meme is saved to the Camera Roll

--- moomood/ShowMediaViewController.swift
+++ moomood/ShowMediaViewController.swift
@@ -3,28 +3,25 @@ import UIKit
 class ShowMediaViewController: UIViewController {
     var image: UIImage? = nil
     var titreText: String!
+    
+    @IBOutlet weak var imageView: UIImageView!
   
     @IBAction func saveToLibrary(_ sender: UIBarButtonItem) {
         UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
     
-    //MARK: - Add image to Library
     @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error = error {
-            // we got back an error!
             let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             present(ac, animated: true)
         } else {
-            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            let ac = UIAlertController(title: "Awesome!", message: "The meme has been saved to your Photos.", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             present(ac, animated: true)
         }
     }
-    
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if image != nil {
