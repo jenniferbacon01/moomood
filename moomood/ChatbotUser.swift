d21cd014dbc56af19ed9a2e213d170e0afba775e
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-10-19 09:55:00 +0100

    chatbot user refactored, chatbot recognises username

---
+++ moomood/ChatbotUser.swift
@@ -0,0 +1,19 @@
+//
+//  ChatbotUser.swift
+//  moomood
+//
+//  Created by USMAC on 19/10/2017.
+//  Copyright © 2017 Jennifer Bacon. All rights reserved.
+//
+
+import Foundation
+
+class ChatbotUser {
+    let id: String
+    let name: String
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
