6537f14c0bfebfb3eb726e5a71af80d9ef658edb
Jennifer Bacon <jenniferbacon01@gmail.com>
2017-10-19 10:28:49 +0100

    moved setgradient function for buttons to moodinputviewdisplay

--- moomood/MoodInputViewDisplay.swift
+++ moomood/MoodInputViewDisplay.swift
@@ -1,30 +1,38 @@
 import Foundation
 import UIKit
 
-class MoodInputViewDisplay {
+class MoodInputViewDisplay: UIView {
 
     func displayChosenMoodMessage(number: Int, chosenMoodMessage: UILabel) {
         let messageString = "you chose "
         chosenMoodMessage.text = (messageString + String(number))
     }
-    
     func hideButtons(buttons: [UIButton]){
         for i in (0..<buttons.count) {
             buttons[i].isHidden = true
         }
     }
-    
     func showButtons(buttons: [UIButton]){
         for i in (0..<buttons.count) {
             buttons[i].isHidden = false
         }
     }
-    
     func formatButton(button: UIButton, colorOne: UIColor, colorTwo: UIColor){
         button.layer.cornerRadius = 10
         button.layer.masksToBounds = true
         button.setGradientBackground(colorOne: colorOne, colorTwo: colorTwo)
     }
+}
+
+extension UIView {
     
-    
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
 }
